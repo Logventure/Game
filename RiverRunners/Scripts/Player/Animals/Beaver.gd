e51a0ce715996a620a55855b2ee01e9e03410843
inessantos3 <113998154+inessantos3@users.noreply.github.com>
2023-10-31 15:38:53 +0000

    Updated sprites

--- RiverRunners/Scripts/Player/Animals/Beaver.gd
+++ RiverRunners/Scripts/Player/Animals/Beaver.gd
@@ -1,22 +1,17 @@
 extends Sprite2D
 
 var animal
-#var destination = Vector2.ZERO
-#var aux1 = Vector2.ZERO #variavel auxiliar
-#var aux2 = Vector2.ZERO #variavel auxiliar
 var pos = Vector2.ZERO
 var gravity = 400
 var time = 0
 var speed = 500
 var delay = 0.12
 var is_jumping = false
-#var array = {}
-#var array1 = {}
 
 func _ready():
 	animal = Animal.new() 
 	pos = position.y
-	Events.connect("input_jump", jump)
+	#Events.connect("input_jump", jump)
 
 func jump():
 	if not is_jumping:
@@ -29,6 +24,8 @@ func handle_jump(delta):
 	if is_jumping && position.y <= pos && time >= 0:
 		position.y = pos - (speed + gravity * time * -1) * time 
 	elif time >= 0:
+		if(is_jumping):	#acabou o salto
+			pass
 		is_jumping = false
 		position.y = pos
 	
--- RiverRunners/Scripts/Player/Animals/Crab.gd
+++ RiverRunners/Scripts/Player/Animals/Crab.gd
@@ -1,19 +1,34 @@
 extends Sprite2D
 
 var animal
-var destination = Vector2.ZERO
-var deltaTime = 0
-var delay = 1
+var pos = Vector2.ZERO
+var gravity = 400
+var time = 0
+var speed = 500
+var delay = 0.12
+var is_jumping = false
 
 func _ready():
-    animal = Animal.new() 
-    Events.connect("input_jump", jump)
+	animal = Animal.new() 
+	pos = position.y
+	Events.connect("input_jump", jump)
 
 func jump():
-    var array
-    array = animal.jump(position, delay)
-    destination = array[0]
-    deltaTime = array[1]
+	if not is_jumping:
+		is_jumping = true
+		time = -1*delay
+
+
+func handle_jump(delta): 
+	time += delta
+	if is_jumping && position.y <= pos && time >= 0:
+		position.y = pos - (speed + gravity * time * -1) * time 
+	elif time >= 0:
+		if(is_jumping):	#acabou o salto
+			pass
+		is_jumping = false
+		position.y = pos
+	
 
 func _process(delta):
-    position = position.move_toward(destination, deltaTime)
+	handle_jump(delta)

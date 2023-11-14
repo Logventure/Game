717277251fbb018a9df757d96926e5c911cd5e1d
inessantos3 <113998154+inessantos3@users.noreply.github.com>
2023-11-14 16:54:56 +0000

    working on game manager

--- RiverRunners/Scripts/Game/GameManager.gd
+++ RiverRunners/Scripts/Game/GameManager.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+enum States {MAIN_MENU, OPTIONS, LEVEL_SELECT, LEVEL, LOADING_SCREEN}
+
+var current_state = States.MAIN_MENU
+
+var target_state = States.MAIN_MENU 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -7,6 +13,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	match current_state:
+		States.MAIN_MENU:
+			pass
+
+		States.OPTIONS:
+			pass
+
+		States.LEVEL_SELECT:
+			pass
 	
+		States.LEVEL:
+			pass
+
+		States.LOADING_SCREEN:
+			pass
 	
+func switchToMainMenu():
+	current_state = States.MAIN_MENU
+
+
+func switchToLevelSelect():
+	current_state = States.LEVEL_SELECT
+		

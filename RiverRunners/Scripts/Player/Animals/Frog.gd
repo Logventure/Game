1a2815d8ed9c9534b24afa1e7e0b14a38daf7950
Inês Santos <ismsantos@ua.pt>
2023-12-19 11:51:14 +0000

    fixed level not ending bug + jump sfx bug

--- RiverRunners/Scripts/Player/Animals/Frog.gd
+++ RiverRunners/Scripts/Player/Animals/Frog.gd
@@ -5,7 +5,7 @@ var pos = Vector2.ZERO
 var gravity = 400
 var time = 0
 var speed = 500
-var delay = 0.24
+var delay = 0.12
 var is_jumping = false
 var logNode
 var basePosition = position
@@ -37,7 +37,12 @@ func _ready():
 func jump():
 	if canJump:
 		current_state = States.JUMPING
-		time = -1*delay
+		var number_of_animals = 0
+		if get_node("../").isCharacterAvailable("crab"):
+			number_of_animals += 1
+		if get_node("../").isCharacterAvailable("otter"):
+			number_of_animals += 1
+		time = -1*delay*number_of_animals
 		pos = logNode.position.y + basePosition.y
 		Events.emit_signal("is_on_air", true)
 		loseDamage = true
@@ -77,31 +82,32 @@ func _process(delta):
 		visible = char_available
 		collider.monitoring = visible
 
-	var commands = InputHandler.getCommands()
-	match current_state:
-		States.IDLE:
-			handle_position()
-			if len(commands) > 0:
-				if commands.find("jump") != -1 and get_node("../").isCharacterAvailable("frog") and not get_node("../").isMoving():
-					jump()
-			else:
-				var last_input = InputHandler.getLastInput()
-				if last_input == "jump" and get_node("../").isCharacterAvailable("frog") and not get_node("../").isMoving():
-					jump()
-					InputHandler.clearLastInput()
+	if get_node("../").isCharacterAvailable("frog"):
+		var commands = InputHandler.getCommands()
+		match current_state:
+			States.IDLE:
+				handle_position()
+				if len(commands) > 0:
+					if commands.find("jump") != -1 and get_node("../").isCharacterAvailable("frog") and not get_node("../").isMoving():
+						jump()
+				else:
+					var last_input = InputHandler.getLastInput()
+					if last_input == "jump" and get_node("../").isCharacterAvailable("frog") and not get_node("../").isMoving():
+						jump()
+						InputHandler.clearLastInput()
 
-		States.JUMPING:
-			handle_jump(delta)
+			States.JUMPING:
+				handle_jump(delta)
 
-		States.DROWNING:
-			current_state = States.IDLE
+			States.DROWNING:
+				current_state = States.IDLE
 
-		States.TAKE_DAMAGE:
-			pass
+			States.TAKE_DAMAGE:
+				pass
 
-		
-		States.PAUSED:
-			pass
+			
+			States.PAUSED:
+				pass
 
 func onPause():
 	previous_state = current_state

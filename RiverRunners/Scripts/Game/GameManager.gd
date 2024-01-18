1ae5cfaa6e6be3e5ee031ca118bd74055ece84ca
Daniel <41786@ufp.edu.pt>
2024-01-18 02:12:56 +0000

    Changed default sounds to half

--- RiverRunners/Scripts/Game/GameManager.gd
+++ RiverRunners/Scripts/Game/GameManager.gd
@@ -181,21 +181,21 @@ func setSound():
 			else:
 				match i:
 					0:
-						AudioServer.set_bus_volume_db(0, linear_to_db(1))
+						AudioServer.set_bus_volume_db(0, linear_to_db(0.5))
 					1:
-						AudioServer.set_bus_volume_db(2, linear_to_db(1))
+						AudioServer.set_bus_volume_db(2, linear_to_db(0.5))
 					2:
-						AudioServer.set_bus_volume_db(3, linear_to_db(1))
+						AudioServer.set_bus_volume_db(3, linear_to_db(0.5))
 					3:	
-						AudioServer.set_bus_volume_db(1, linear_to_db(1))
+						AudioServer.set_bus_volume_db(1, linear_to_db(0.5))
 		else:
 			match i:
 				0:
-					AudioServer.set_bus_volume_db(0, linear_to_db(1))
+					AudioServer.set_bus_volume_db(0, linear_to_db(0.5))
 				1:
-					AudioServer.set_bus_volume_db(2, linear_to_db(1))
+					AudioServer.set_bus_volume_db(2, linear_to_db(0.5))
 				2:
-					AudioServer.set_bus_volume_db(3, linear_to_db(1))
+					AudioServer.set_bus_volume_db(3, linear_to_db(0.5))
 				3:	
-					AudioServer.set_bus_volume_db(1, linear_to_db(1))
+					AudioServer.set_bus_volume_db(1, linear_to_db(0.5))
 		i +=1
